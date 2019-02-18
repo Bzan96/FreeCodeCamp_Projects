8582fe8bada85ca93b92983a109378383ffe70b5
Bzan96 <bzan96@gmail.com>
2019-02-18 20:18:49 +0100

    Added ability to use Backspace and fixed a bug where a number could contain several decimal points.

--- Front_End_Libraries_section/javascript_calculator/src/components/Calculator.js
+++ Front_End_Libraries_section/javascript_calculator/src/components/Calculator.js
@@ -3,28 +3,35 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Screen from "./Screen";
 
-/** ToDo:
-SEMI-FIXED, except you can only use one decimal ever! =^_^=    
-3. Prevent multiple decimal point in a row (user story 11)
-ToDo: Add a clause to handle the total ending with +*-/
+/** Bug:
+If you, for some reason, want to write 00000.00000, you can, but
+that should really be reduced to 0.00000 automatically to save space.
 */
 
 class Calculator extends React.Component {
     constructor() {
         super()
         this.state = {
-            total: ""
+            total: "",
         }
     }
 
     calculate = () => {
         // Eval is an unsafe function that shouldn't be used.
         // It's "safe" in this case because they user can't input custom strings.
-        let result = (eval(this.state.total) || "" ) + ""
-        let decimals = result.toString().split(".")[1]
-        console.log(result)
-        console.log(typeof result)
-            
+        let result = "";
+        let decimals = "";
+        
+        if(this.state.total[this.state.total.length-1].match(/\D/) ) {
+            result = (eval(this.state.total.slice(0, this.state.total.length-1)) || "" ) + ""
+            decimals = result.toString().split(".")[1]
+        } else {
+            result = (eval(this.state.total) || "" ) + ""
+            decimals = result.toString().split(".")[1]
+        }
+
+
+
         try {
             if(result.includes(".") && decimals.length > 4) {
                 // Technically not correct math-wise since a number 5 or up doesn't round up.
@@ -51,14 +58,19 @@ class Calculator extends React.Component {
         })
     }
 
-    handleClick = (event) => {
-        const value = event.target.getAttribute("value")
-        let lastSegment = this.state.total.slice(-this.state.total.indexOf( ("/" || "*" || "-" || "+") ), this.state.total.length)
+    handleValue = (value) => {
+        let splitByOperator = this.state.total.split(/(-?\d+\.?\d*)/)
+        let last = splitByOperator[splitByOperator.length-2]
 
         if(value === "=") {
             this.calculate()
         } else if(value === "C") {
             this.reset()
+        } else if(this.state.total.length >= 15) {
+            this.setState({
+                total: this.state.total
+            })
+            alert("The calculator doesn't allow more characters, sorry.");
         } else if(value === "0") {
             if(this.state.total === "") {
                 this.setState({
@@ -70,41 +82,27 @@ class Calculator extends React.Component {
                 })
             }
         } else if(value === ".") {
-            /* split the segment
-                if the last/current segment contains a decimal point, ignore the last input
-                if it doesn't, allow one decimal point
-                allow one decimal point per split segment
-                if the last input is /*+- and value is . insert a 0 first */
-            // const regex = /\D(!.)/;
-
-            let splitTot = this.state.total.split("/" || "*" || "-" || "+");
-            // let lastSegment = splitTot[splitTot.length-1];
-
-            console.log(this.state.total.slice(-this.state.total.indexOf( ("/" || "*" || "-" || "+") ), this.state.total.length) )
-            console.log(splitTot);
-            console.log(lastSegment)
-            console.log(lastSegment.split("") )
-            
             if(this.state.total === "") {
                 this.setState({
                     total: "0."
                 })
-            } else if(lastSegment === "") {
+            } else if(this.state.total.match(/(\D)$/) && this.state.total.split(this.state.total.length-3, this.state.total-1) !== "0.") {
                 this.setState({
                     total: this.state.total + "0" + value
                 })
-                // Doesn't register
-            } else if(!lastSegment.includes(".") ) {
-                this.setState({
-                    total: this.state.total + value
-                })
-            } else if(lastSegment.includes(".") ) {
+            } else if(last.includes(".") ) {
                 this.setState({
                     total: this.state.total
+                })
+            } else {
+                this.setState({
+                    total: this.state.total + value
                 })
             }
         } else if(value === "/" || value === "*" || value === "-" || value === "+") {
             let lastInput = this.state.total.split("")[this.state.total.length-1];
+            
+            
             if(this.state.total === "") {
                 this.setState({
                     total: "0" + value
@@ -125,6 +123,38 @@ class Calculator extends React.Component {
         }
     }
 
+    handleClick = (event) => {
+        const value = event.target.getAttribute("value");
+
+        this.handleValue(value);
+    }
+
+    // Keyboard presses
+    handleKeyDown = (event) => {
+        const symbols = ["0","1","2","3","4","5","6","7","8","9","/","-","*","+","."];
+        if(symbols.includes(event.key) ) {
+            this.handleValue(event.key);
+        } else if(event.key === ",") { // On a Swedish keyboard, the "." key on a numpad is a ","
+            this.handleValue(".");
+        } else if(event.key === "Enter") {
+            this.calculate();
+        } else if(event.key === "c") {
+            this.reset();
+        } else if(event.key === "Backspace") {
+            this.setState({
+                total: this.state.total.slice(0, this.state.total.length-1)
+            })
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown", this.handleKeyDown)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyDown)
+    }
+
     render() {
         return(
             <div>
@@ -134,6 +164,7 @@ class Calculator extends React.Component {
                     handleClick={this.handleClick}
                     value={this.value}
                     id={this.id}
+                    onKeyDown={this.onKeyDown}
                 />
                 <Footer />
             </div>
